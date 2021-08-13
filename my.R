6788848bbd5e3b8d4a308f5651b2da967f0bf1eb
TaeTae <hc07car@gmail.com>
2021-08-13 15:39:26 +0900

    edit code

--- my.R
+++ my.R
@@ -1,4 +1,9 @@
-setwd(r'(C:\Users\hyeon\Desktop\Hyeon\Finance_competition\data\서울시 지역단위(집계구 업데이트))')
+#### 나중에 할 것들
+## library  맨위로 빼기
+
+#setwd(r'(C:\Users\hyeon\Desktop\Hyeon\Finance_competition\data\서울시 지역단위(집계구 업데이트))')
+setwd("C:/Users/TaeyunKim/Documents/R_script/FInance_data")
+library(dplyr)
 library(readxl)
 #거리두기 단계를 19년 3월, 9월은 0 / 20년 3월은 1 / 20년 9월과 21년 3월은 3으로 줌
 shinhan1903 <- read_excel("신한은행201903.xlsx")
@@ -13,51 +18,47 @@ shinhan2103 <- read_excel("신한은행202103.xlsx")
 shinhan2103$거리두기 <- 3
 shinhan <- rbind(shinhan1903, shinhan1909, shinhan2003, shinhan2009, shinhan2103)
 shinhan <- as.data.frame(shinhan)
-head(shinhan)
-tail(shinhan)
+# 메모리 아끼기 위해서 나머지 변수 다 지움.
+rm(shinhan1903); rm(shinhan1909); rm(shinhan2003); rm(shinhan2009);rm(shinhan2103)
 names(shinhan)
 shinhan_d <- shinhan[,c("지역구", "나이", "성별", "직장인여부", "총소비금액", "총수신금액", "거리두기")]
-
+shinhan_d$지역구 <- as.factor(shinhan_d$지역구) 
+shinhan_d$나이 <- as.factor(shinhan_d$나이) 
+shinhan_d$성별 <- as.factor(shinhan_d$성별)
+shinhan_d$직장인여부 <- as.factor(shinhan_d$직장인여부)
+shinhan_d$거리두기 = as.factor(shinhan_d$거리두기)
+rm(shinhan)
 #training과 test 데이터 나누기
 #80% : 20%로 나눔
 #1039568*0.8에서 올림해서 training 데이터에 넣음
-
 set.seed(1)
 random <- sample(1:1039568, 831655)
 shinhan_tr <- shinhan_d[random, ]
 shinhan_ts <- shinhan_d[-random, ]
 
 #단순선형회귀
-shinhan_lm <- lm(총소비금액 ~ factor(지역구) + factor(나이) + factor(성별)
-                      + factor(직장인여부) + 총수신금액 + 거리두기, data = shinhan_tr)
+shinhan_lm <- lm(총소비금액 ~ 지역구 + 나이 + 성별
+                      + 직장인여부 + 총수신금액 + 거리두기, data = shinhan_tr)
 summary(shinhan_lm)
 lm_pr <- predict(shinhan_lm, shinhan_ts)
-plot(shinhan_ts$총소비금액 - lm_pr) #오차
+head(lm_pr)
 
 
 #ridge, lasso regression
 ##범주형변수 변환 후 더미변수 만들기
-install.packages("mltools")
+
 library(mltools)
 library(data.table)
-shinhan_d$지역구 <- as.factor(shinhan_d$지역구) 
-shinhan_d$나이 <- as.factor(shinhan_d$나이) 
-shinhan_d$성별 <- as.factor(shinhan_d$성별)
-shinhan_d$직장인여부 <- as.factor(shinhan_d$직장인여부)
 
-fac <- one_hot(as.data.table(shinhan_d))
-head(fac)
-str(fac)
+OnehotShinhan_tr <- one_hot(as.data.table(shinhan_tr))
 
-#이전과 같이 training과 test data 나눔
-set.seed(1)
-random <- sample(1:1039568, 831655)
-shinhan_tr <- fac[random, ]
-shinhan_ts <- fac[-random, ]
+OnehotShinhan_ts <- one_hot(as.data.table(shinhan_ts))
 
+
+#이전과 같이 training과 test data 나눔 ==> 어차피 나눴는데 그대로 사용하면 되지않나?
 library(glmnet)
-shinhan_tr_x <- as.matrix(shinhan_tr[, -36])
-shinhan_tr_y <- as.matrix(shinhan_tr[, 36])
+shinhan_tr_x <- as.matrix(OnehotShinhan_tr %>% select(-'총소비금액'))
+shinhan_tr_y <- as.matrix(OnehotShinhan_tr %>% select('총소비금액'))
 
 fit_lasso <- cv.glmnet(shinhan_tr_x, shinhan_tr_y, family = "gaussian")
 fit_ridge <- cv.glmnet(shinhan_tr_x, shinhan_tr_y, family = "gaussian", alpha = 0)
@@ -65,8 +66,8 @@ fit_ridge <- cv.glmnet(shinhan_tr_x, shinhan_tr_y, family = "gaussian", alpha = 
 coef(fit_lasso)
 coef(fit_ridge)
 
-pr_las <- predict(fit_lasso, as.matrix(shinhan_ts[, -36]))
-pr_rid <- predict(fit_ridge, as.matrix(shinhan_ts[, -36]))
+pr_las <- predict(fit_lasso, as.matrix(OnehotShinhan_ts %>% select(-'총소비금액')))
+pr_rid <- predict(fit_ridge, as.matrix(OnehotShinhan_ts %>% select(-"총소비금액")))
 yy <- shinhan_ts$총소비금액 - pr_las
 kk <- shinhan_ts$총소비금액 - pr_rid
 plot(yy)
