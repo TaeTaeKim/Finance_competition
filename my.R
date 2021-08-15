8f67eae06ad0c362eccb547f0953b7731b7fe15e
TaeTae <hc07car@gmail.com>
2021-08-15 19:03:01 +0900

    Branch arrangement

--- my.R
+++ my.R
@@ -1,10 +1,16 @@
-#### 나중에 할 것들
-## library  맨위로 빼기
+#######################각 데이터 불러오기
+library(dplyr)
+library(readxl)
+
+
+######### working directory설정
 
 #setwd(r'(C:\Users\hyeon\Desktop\Hyeon\Finance_competition\data\서울시 지역단위(집계구 업데이트))')
 setwd("C:/Users/TaeyunKim/Documents/R_script/FInance_data")
-library(dplyr)
-library(readxl)
+
+
+# ===========1. 신한은행 데이터 로드=============
+
 #거리두기 단계를 19년 3월, 9월은 0 / 20년 3월은 1 / 20년 9월과 21년 3월은 3으로 줌
 shinhan1903 <- read_excel("신한은행201903.xlsx")
 shinhan1903$거리두기 <- 0
@@ -20,54 +26,13 @@ shinhan <- rbind(shinhan1903, shinhan1909, shinhan2003, shinhan2009, shinhan2103
 shinhan <- as.data.frame(shinhan)
 # 메모리 아끼기 위해서 나머지 변수 다 지움.
 rm(shinhan1903); rm(shinhan1909); rm(shinhan2003); rm(shinhan2009);rm(shinhan2103)
-names(shinhan)
-shinhan_d <- shinhan[,c("지역구", "나이", "성별", "직장인여부", "총소비금액", "총수신금액", "거리두기")]
-shinhan_d$지역구 <- as.factor(shinhan_d$지역구) 
-shinhan_d$나이 <- as.factor(shinhan_d$나이) 
-shinhan_d$성별 <- as.factor(shinhan_d$성별)
-shinhan_d$직장인여부 <- as.factor(shinhan_d$직장인여부)
-shinhan_d$거리두기 = as.factor(shinhan_d$거리두기)
-rm(shinhan)
-#training과 test 데이터 나누기
-#80% : 20%로 나눔
-#1039568*0.8에서 올림해서 training 데이터에 넣음
-set.seed(1)
-random <- sample(1:1039568, 831655)
-shinhan_tr <- shinhan_d[random, ]
-shinhan_ts <- shinhan_d[-random, ]
-
-#단순선형회귀
-shinhan_lm <- lm(총소비금액 ~ 지역구 + 나이 + 성별
-                      + 직장인여부 + 총수신금액 + 거리두기, data = shinhan_tr)
-summary(shinhan_lm)
-lm_pr <- predict(shinhan_lm, shinhan_ts)
-head(lm_pr)
 
 
-#ridge, lasso regression
-##범주형변수 변환 후 더미변수 만들기
 
-library(mltools)
-library(data.table)
+# ===========2. 비씨카드 온라인 매출 데이터 로드=============
+online <- read.csv("온라인 품목별 소비데이터.csv")
 
-OnehotShinhan_tr <- one_hot(as.data.table(shinhan_tr))
-
-OnehotShinhan_ts <- one_hot(as.data.table(shinhan_ts))
+# ===========3. 한국 투자증권 데이터 로드=============
 
 
-#이전과 같이 training과 test data 나눔 ==> 어차피 나눴는데 그대로 사용하면 되지않나?
-library(glmnet)
-shinhan_tr_x <- as.matrix(OnehotShinhan_tr %>% select(-'총소비금액'))
-shinhan_tr_y <- as.matrix(OnehotShinhan_tr %>% select('총소비금액'))
-
-fit_lasso <- cv.glmnet(shinhan_tr_x, shinhan_tr_y, family = "gaussian")
-fit_ridge <- cv.glmnet(shinhan_tr_x, shinhan_tr_y, family = "gaussian", alpha = 0)
-
-coef(fit_lasso)
-coef(fit_ridge)
-
-pr_las <- predict(fit_lasso, as.matrix(OnehotShinhan_ts %>% select(-'총소비금액')))
-pr_rid <- predict(fit_ridge, as.matrix(OnehotShinhan_ts %>% select(-"총소비금액")))
-yy <- shinhan_ts$총소비금액 - pr_las
-kk <- shinhan_ts$총소비금액 - pr_rid
-plot(yy)
+# ===========4. 서울시 가맹점 데이터 로드=============
