3880f8d224c33a71043005ca3535611c8889a3ec
hyeon13 <hyeon1340@gmail.com>
2021-08-26 02:46:12 +0900

    Modified and Add regression

--- onlinedata.R
+++ onlinedata.R
@@ -1,22 +1,18 @@
-#setwd(r'(C:\Users\hyeon\Desktop\Hyeon\Finance_competition\data)')
-
-
-
 ## 워킹디렉토리는 my.r에서 설정하기!!
-
 
 online$거리두기 <- ifelse(online$기준년월 %in% c(201903, 201909), 0, 
                       ifelse(online$기준년월 == 202003, 1, 3))
-online <- online[, c("품목중분류명", "성별", "연령", "고객소재지_시군구", "매출금액","거리두기")]
+online <- online[, c("품목중분류명", "성별", "연령", "매출금액","거리두기")]
+#고객소재지_시군구 제외함
 
 online$품목중분류명 <- factor(online$품목중분류명)
 online$성별 <- factor(online$성별)
 online$연령 <- factor(online$연령)
-online$고객소재지_시군구 <- factor(online$고객소재지_시군구)
+#online$고객소재지_시군구 <- factor(online$고객소재지_시군구)
 online$거리두기 <- factor(online$거리두기)
 
 #지역을 서울로 한정
-online <- online[!(online$고객소재지_시군구 %in% c('용인시', '수원시', '고양시', '서구', '남동구', '부평구', '강화군', '계양구', '성남시', '미추홀구', '연수구', '동구', '옹진군')), ]
+#online <- online[!(online$고객소재지_시군구 %in% c('용인시', '수원시', '고양시', '서구', '남동구', '부평구', '강화군', '계양구', '성남시', '미추홀구', '연수구', '동구', '옹진군')), ]
 
 sp <- sample(1:nrow(online), ceiling(nrow(online)*0.8))
 
@@ -59,3 +55,16 @@ yy <- online_ts$매출금액 - pr_las
 kk <- online_ts$매출금액 - pr_rid
 plot(yy)
 yy
+
+
+#----------------------------------#
+
+##코로나전후  나눠서 회귀분석
+online_bc <- online %>% filter(거리두기 %in% '0') %>% select(품목중분류명, 성별, 연령, 매출금액)
+online_ac <- online %>% filter(거리두기 %in% c('1', '3')) %>% select(품목중분류명, 성별, 연령, 매출금액)
+
+lm_online_bc <- lm(매출금액 ~ ., data = online_bc)
+lm_online_ac <- lm(매출금액 ~ ., data = online_ac)
+
+summary(lm_online_bc)
+summary(lm_online_ac)
